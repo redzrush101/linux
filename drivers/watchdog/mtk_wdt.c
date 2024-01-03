4ee195954e4499acece95cc759cebc8ed2d9f09b
Arseniy Velikanov <me@adomerle.xyz>
2024-01-03 08:31:32 +0400

    watchdog: mtk_wdt: Add support for MT6765 WDT
    Add support for watchdog and reset generator unit of the MediaTek
    MT6765 SoC.
    
    Signed-off-by: Arseniy Velikanov <me@adomerle.xyz>

--- drivers/watchdog/mtk_wdt.c
+++ drivers/watchdog/mtk_wdt.c
@@ -11,6 +11,7 @@
 
 #include <dt-bindings/reset/mt2712-resets.h>
 #include <dt-bindings/reset/mediatek,mt6735-wdt.h>
+#include <dt-bindings/reset/mediatek,mt6765-resets.h>
 #include <dt-bindings/reset/mediatek,mt6795-resets.h>
 #include <dt-bindings/reset/mt7986-resets.h>
 #include <dt-bindings/reset/mt8183-resets.h>
@@ -90,6 +91,9 @@ static const struct mtk_wdt_data mt2712_data = {
 
 static const struct mtk_wdt_data mt6735_data = {
 	.toprgu_sw_rst_num = MT6735_TOPRGU_RST_NUM,
+};
+static const struct mtk_wdt_data mt6765_data = {
+	.toprgu_sw_rst_num = MT6765_TOPRGU_RST_NUM,
 };
 
 static const struct mtk_wdt_data mt6795_data = {
@@ -495,6 +499,7 @@ static const struct of_device_id mtk_wdt_dt_ids[] = {
 	{ .compatible = "mediatek,mt2712-wdt", .data = &mt2712_data },
 	{ .compatible = "mediatek,mt6589-wdt" },
 	{ .compatible = "mediatek,mt6735-wdt", .data = &mt6735_data },
+	{ .compatible = "mediatek,mt6765-wdt", .data = &mt6765_data },
 	{ .compatible = "mediatek,mt6795-wdt", .data = &mt6795_data },
 	{ .compatible = "mediatek,mt7986-wdt", .data = &mt7986_data },
 	{ .compatible = "mediatek,mt7988-wdt", .data = &mt7988_data },
