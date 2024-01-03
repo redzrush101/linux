1255d3a4638259f43a9494de399e808cb8af4eda
Arseniy Velikanov <me@adomerle.xyz>
2024-01-03 07:25:40 +0400

    iommu/mediatek: Add support for MT6765 M4U
    Add IOMMU support for MT6765 SoC.
    
    Signed-off-by: Arseniy Velikanov <me@adomerle.xyz>

--- drivers/iommu/mtk_iommu.c
+++ drivers/iommu/mtk_iommu.c
@@ -167,6 +167,7 @@
 
 enum mtk_iommu_plat {
 	M4U_MT2712,
+	M4U_MT6765,
 	M4U_MT6779,
 	M4U_MT6795,
 	M4U_MT8167,
@@ -1564,6 +1565,17 @@ static const struct dev_pm_ops mtk_iommu_pm_ops = {
 				     pm_runtime_force_resume)
 };
 
+static const struct mtk_iommu_plat_data mt6765_data = {
+	.m4u_plat      = M4U_MT6765,
+	.flags         = MTK_IOMMU_TYPE_MM | RESET_AXI | WR_THROT_EN | IOVA_34_EN | PGTABLE_PA_35_EN,
+	.inv_sel_reg   = REG_MMU_INV_SEL_GEN1,
+	.banks_num    = 1,
+	.banks_enable = {true},
+	.iova_region   = single_domain,
+	.iova_region_nr = ARRAY_SIZE(single_domain),
+	.larbid_remap = {{0}, {1}, {2}, {3}},
+};
+
 static const struct mtk_iommu_plat_data mt2712_data = {
 	.m4u_plat     = M4U_MT2712,
 	.flags        = HAS_4GB_MODE | HAS_BCLK | HAS_VLD_PA_RNG | SHARE_PGTABLE |
@@ -1901,6 +1913,7 @@ static const struct mtk_iommu_plat_data mt8365_data = {
 
 static const struct of_device_id mtk_iommu_of_ids[] = {
 	{ .compatible = "mediatek,mt2712-m4u", .data = &mt2712_data},
+	{ .compatible = "mediatek,mt6765-m4u", .data = &mt6765_data},
 	{ .compatible = "mediatek,mt6779-m4u", .data = &mt6779_data},
 	{ .compatible = "mediatek,mt6795-m4u", .data = &mt6795_data},
 	{ .compatible = "mediatek,mt6893-iommu-mm", .data = &mt6893_data},
